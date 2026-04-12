c0af0570a77bf80df5903b67e029df049cbc7bb2
Odunola Akimu Mosebolatan <146633788+kimicds@users.noreply.github.com>
2026-04-12 12:32:22 +0100

    Delete static directory

--- static/script.js
+++
@@ -1,6 +0,0 @@
-navigator.geolocation.getCurrentPosition(function(pos){
-
-document.getElementById("latitude").value = pos.coords.latitude
-document.getElementById("longitude").value = pos.coords.longitude
-
-})
